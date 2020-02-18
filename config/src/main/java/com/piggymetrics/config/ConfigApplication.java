26de6009a4bde8630980f08ede969177d0f2b55a
Bugra SESSEVMZ <sessevmezbugra@gmail.com>
2020-02-18 22:09:03 +0300

    535353

--- config/src/main/java/com/piggymetrics/config/ConfigApplication.java
+++ config/src/main/java/com/piggymetrics/config/ConfigApplication.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 @SpringBootApplication
 @EnableConfigServer
 public class ConfigApplication {
-
+//TESTTTTTTT
 	public static void main(String[] args) {
 		SpringApplication.run(ConfigApplication.class, args);
 	}
